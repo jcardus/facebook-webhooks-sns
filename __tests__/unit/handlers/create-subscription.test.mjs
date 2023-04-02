15fe064ab6525b02e4f14f7843c740623b410853
jcardus <asklocation.net@gmail.com>
2023-04-02 17:40:01 -0400

    start sns

--- __tests__/unit/handlers/create-subscription.test.mjs
+++ __tests__/unit/handlers/create-subscription.test.mjs
@@ -1,16 +1,25 @@
-import { get } from '../../../src/handlers/index.mjs';
+import { get, post } from '../../../src/handlers/index.mjs';
 import event from '../../../events/create-subscription.json'
+import leadgen from '../../../events/leadgen.json'
 
-describe('Test createSubscription', function () {
+describe('Test webhooks', function () {
     it('should verify', async () => {
         const challenge = '1158201444'
         event.queryStringParameters['hub.challenge'] = challenge
-        // Invoke putItemHandler()
         const result = await get(event);
 
         const expectedResult = {
             statusCode: 200,
             body: challenge
+        };
+        expect(result).toEqual(expectedResult);
+    });
+
+    test('leadgen', async () => {
+        const result = await post(leadgen);
+
+        const expectedResult = {
+            statusCode: 200
         };
         expect(result).toEqual(expectedResult);
     });
--- src/handlers/index.mjs
+++ src/handlers/index.mjs
@@ -1,21 +1,36 @@
-
-/**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
- */
+import {PublishCommand, SNSClient} from "@aws-sdk/client-sns";
+const region = process.env.AWS_REGION
+const snsClient = new SNSClient({ region });
 
 export const get = async (event) => {
     if (event.httpMethod !== 'GET') {
         throw new Error(`this only accepts GET method, you tried: ${event.httpMethod} method.`);
     }
-    // All log statements are written to CloudWatch
     console.info('received:', event);
-
     const response = {
         statusCode: 200,
         body: event.queryStringParameters['hub.challenge']
-    };
-
-    // All log statements are written to CloudWatch
+    }
     console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
-};
+    return response
+}
+
+export const post = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`this only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+    const body = JSON.parse(event.body)
+    for (const e of body.entry) {
+        for (const change of e.changes) {
+            const TopicArn = process.env[`TOPIC_${change.field.toUpperCase()}`]
+            console.log('publish', change.value, 'to', TopicArn)
+            await snsClient.send(new PublishCommand({
+                Message: JSON.stringify(change.value),
+                TopicArn
+            }))
+        }
+    }
+    const response = { statusCode: 200 }
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`)
+    return response
+}
